933baf65eb140eb4e377bb9cb33748ddfdb9b760
pedrofranq96 <pedrofranq96@gmail.com>
2024-08-04 18:01:28 -0300

    Configurações inicias, configurando a documentação do swagger

---
+++ Fina.Api/Program.cs
@@ -0,0 +1,53 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => {
+    x.CustomSchemaIds(n => n.FullName);
+});
+builder.Services.AddTransient<Handler>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("/v1/transactions", 
+    (Request request, Handler handler) => handler.Handle(request)).WithName("transactions: Create")
+                                                                  .WithSummary("Cria uma nova transação")
+                                                                  .Produces<Response>();
+
+app.Run();
+
+
+
+
+
+
+
+
+public class Request {
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public string UserId  { get; set; } = string.Empty;
+}
+
+
+public class Response{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
+
+
+public class Handler {
+    public Response Handle(Request request){
+    return new Response{
+        Id = 4,
+        Title = request.Title
+    };
+}
+}
+
+
---
+++ Fina.Core/Enums/ETransactionType.cs
@@ -0,0 +1,8 @@
+namespace Fina.Core.Enums
+{
+    public enum ETransactionType
+    {
+        Deposit = 1,
+        Whithdraw = 2
+    }
+}
